ad85511f918f338c78ef22a18a66ca146ba6922a
Designrpros <130706554+Designrpros@users.noreply.github.com>
2023-05-31 15:51:06 +0200

    blogpost nice

--- src/Pages/Blog/BlogPost.js
+++ src/Pages/Blog/BlogPost.js
@@ -2,6 +2,28 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { db } from '../../FirebaseSDK.js';
 import { doc, getDoc } from 'firebase/firestore';
+import styled from 'styled-components';
+
+const PostContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 80%;
+  margin: auto;
+`;
+
+const PostTitle = styled.h2`
+  /* Add your styles for the title here */
+`;
+
+const PostDate = styled.p`
+  /* Add your styles for the date here */
+`;
+
+const PostContent = styled.p`
+  /* Add your styles for the content here */
+`;
 
 const BlogPost = () => {
   const { postId } = useParams();
@@ -26,11 +48,11 @@ const BlogPost = () => {
   }
 
   return (
-    <div>
-      <h2>{post.title}</h2>
-      <p>{post.date && post.date.toDate().toLocaleDateString()}</p>
-      <p>{post.content}</p>
-    </div>
+    <PostContainer>
+      <PostTitle>{post.title}</PostTitle>
+      <PostDate>{post.date && post.date.toDate().toLocaleDateString()}</PostDate>
+      <PostContent>{post.content}</PostContent>
+    </PostContainer>
   );
 };
 
